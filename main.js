191bccfd8d1c20d7479932838a80e32d60ebf76b
brightyorcerf <thetejaansh@gmail.com>
2026-01-29 02:52:41 +0530

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+const { crawlPage } = require('./crawl.js')
+ 
+async function main(){
+    if(process.argv.length < 3){
+        console.log("no website provided")
+        process.exit(1)
+    }
+    if(process.argv.length > 3){
+        console.log("too many websites submitted")
+        process.exit(1)
+    }
+    const baseURL = process.argv[2] 
+    
+    console.log(`starting crawl of ${baseURL}`) 
+    await crawlPage(baseURL)
+}
+
+main()
